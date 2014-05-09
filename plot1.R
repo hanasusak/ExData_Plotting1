5a3835ae873e254ecb2d9044f386884c4c0997f3
Hana Susak <hana.susak@crg.eu>
2014-05-10 00:14:52 +0200

    first plot

--- plot1.R
+++ plot1.R
@@ -13,3 +13,11 @@ hpc <- hpc[hpc$Date %in% c("1/2/2007", "2/2/2007"),]
 hpc$Date <- as.Date(hpc$Date, "%d/%m/%Y")
 hpc$DateTime <- paste(hpc$Date, hpc$Time)
 hpc$DateTime <- strptime(hpc$DateTime, format="%Y-%m-%d %H:%M:%S")
+
+
+### create and save first PNG plot (with given size)
+png(file = "plot1.png", width = 480, height = 480, units='px', bg = "transparent")
+hist(hpc$Global_active_power, col=2, xlab="Global Active Power (kilowatts)" , 
+     main="Global Active Power", cex.lab=0.9, cex.main=0.9, cex.axis=0.9)
+dev.off()
+
