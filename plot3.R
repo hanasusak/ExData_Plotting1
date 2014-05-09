726f7943e5f88ae638410ef4c8588cc8c8d28fcb
Hana Susak <hana.susak@crg.eu>
2014-05-10 00:33:12 +0200

    third plot

--- plot3.R
+++ plot3.R
@@ -15,7 +15,10 @@ hpc$DateTime <- paste(hpc$Date, hpc$Time)
 hpc$DateTime <- strptime(hpc$DateTime, format="%Y-%m-%d %H:%M:%S")
 
 
-### create and save second PNG plot (with given size)
-png(file = "plot2.png", width = 480, height = 480, units='px', bg = "transparent")
-plot(hpc$DateTime, hpc$Global_active_power, type='l', ylab="Global Active Power (kilowatts)" , xlab="")
+### create and save third PNG plot (with given size)
+png(file = "plot3.png", width = 480, height = 480, units='px', bg = "transparent")
+plot(hpc$DateTime, hpc$Sub_metering_1, type='l', ylab="Energy sub metering" , xlab="")
+lines(hpc$DateTime, hpc$Sub_metering_2, col=2)
+lines(hpc$DateTime, hpc$Sub_metering_3, col=4)
+legend('topright', c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), col=c(1,2,4), lwd = 2)
 dev.off()
