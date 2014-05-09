18fa4833f203f336c90f85cf0f7bdc6f5ed572cb
Hana Susak <hana.susak@crg.eu>
2014-05-10 00:34:29 +0200

    third plot

--- plot3.R
+++ plot3.R
@@ -20,5 +20,5 @@ png(file = "plot3.png", width = 480, height = 480, units='px', bg = "transparent
 plot(hpc$DateTime, hpc$Sub_metering_1, type='l', ylab="Energy sub metering" , xlab="")
 lines(hpc$DateTime, hpc$Sub_metering_2, col=2)
 lines(hpc$DateTime, hpc$Sub_metering_3, col=4)
-legend('topright', c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), col=c(1,2,4), lwd = 2)
+legend('topright', c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), col=c(1,2,4), lwd = 1)
 dev.off()
