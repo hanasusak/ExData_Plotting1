c4300b67d043bf2c5c54b15cc937e18a2986343a
Hana Susak <hana.susak@crg.eu>
2014-05-11 16:11:18 +0200

    final commit

--- plot4.R
+++ plot4.R
@@ -15,10 +15,19 @@ hpc$DateTime <- paste(hpc$Date, hpc$Time)
 hpc$DateTime <- strptime(hpc$DateTime, format="%Y-%m-%d %H:%M:%S")
 
 
-### create and save third PNG plot (with given size)
-png(file = "plot3.png", width = 480, height = 480, units='px', bg = "transparent")
+### create and save fourth PNG plot (with given size)
+png(file = "plot4.png", width = 480, height = 480, units='px', bg = "transparent")
+par(mfrow=c(2,2))
+
+plot(hpc$DateTime, hpc$Global_active_power, type='l', ylab="Global Active Power" , xlab="")
+
+plot(hpc$DateTime, hpc$Voltage, type='l' , xlab="datetime", ylab='Voltage')
+
 plot(hpc$DateTime, hpc$Sub_metering_1, type='l', ylab="Energy sub metering" , xlab="")
 lines(hpc$DateTime, hpc$Sub_metering_2, col=2)
 lines(hpc$DateTime, hpc$Sub_metering_3, col=4)
-legend('topright', c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), col=c(1,2,4), lwd = 1)
+legend('topright', c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), col=c(1,2,4), lwd = 1, box.lwd = 0)
+
+plot(hpc$DateTime, hpc$Global_reactive_power, type='l' ,xlab="datetime", ylab="Global_reactive_power")
+
 dev.off()
